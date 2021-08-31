2672b59c3ce2349220b67072d1b8c853121d344b
Мария <87368091+MariaKorkina@users.noreply.github.com>
2021-08-31 18:26:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,3 @@ function sliderLeft () {
 }
 
 
-/*const timerId = setTimeout(function() {
-    console.log('Hello')
-    
-}, 2000); */
-
-/*let start = new Date ()
-    for (let i = 0; i < 100000; i++) {
-        let some = i ** 3
-    }
-    let end = new Date ()
-    console.log(`Цикл отработал за ${end-start} милисекунд`) */
